a73bf03150098ff4eac05b7dd8a50b78499daf90
kluksu <shriki618@gmail.com>
2022-07-14 18:51:48 +0300

    password_validator.py done

---
+++ password_validator.py
@@ -0,0 +1,36 @@
+
+import re
+from termcolor import colored
+import sys
+password=""
+if(len(sys.argv)>1):
+    password=sys.argv[1]
+regex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\w~@#$%^&*+=`|{}:;!.?\"()\[\]-]{10,}$"
+# regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{10,}$"
+speciel="^(?=.*[!@#$%^&*])"
+capital="^(?=.*[A-Z])"
+lowerCase="^(?=.*[a-z])"
+number="^(?=.*\d)"
+if re.search(regex,password):
+    print(colored("password is valid", 'green'))
+    exit(0)
+
+else:
+    print(colored("password is invalid", 'red'))
+    # if(re.match(speciel,password)):
+    #     print(colored('no special characters are allowd', "red"))
+    if(len(password)<10):
+        print(colored("password must be at least 10 characters long", "red"))
+    if not (re.match(capital, password)):
+        print(colored('your password must include one capital letter at least', "red"))
+    if not (re.match(lowerCase, password)):
+        print(colored('your password must include one lower Case letter at least', "red"))
+    if not (re.match(number, password)):
+        print(colored('your password must include one digit at least', "red"))
+
+    exit(1)
+
+
+
+
+
