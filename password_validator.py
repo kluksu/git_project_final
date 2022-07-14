05abd88f9826558e27e57f27e1ba18f5805384b5
kluksu <shriki618@gmail.com>
2022-07-14 21:04:09 +0300

    regex change + function syntex

--- password_validator.py
+++ password_validator.py
@@ -5,32 +5,38 @@ import sys
 password=""
 if(len(sys.argv)>1):
     password=sys.argv[1]
-regex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\w~@#$%^&*+=`|{}:;!.?\"()\[\]-]{10,}$"
+regex = "^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\w~@#$%^&,*+/=`|{}:;!.?\"()\[\]-]{10,}$"
 # regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{10,}$"
 speciel="^(?=.*[!@#$%^&*])"
 capital="^(?=.*[A-Z])"
 lowerCase="^(?=.*[a-z])"
 number="^(?=.*\d)"
-if re.search(regex,password):
-    print(colored("password is valid", 'green'))
-    exit(0)
+def is_valid(password:str):
+    """
+    receives a string and prints information and exit code about password validation
+    :param password:
+    :return:
+    """
+    if re.search(regex,password):
+        print(colored("password is valid", 'green'))
+        exit(0)
 
-else:
-    print(colored("password is invalid", 'red'))
-    # if(re.match(speciel,password)):
-    #     print(colored('no special characters are allowd', "red"))
-    if(len(password)<10):
-        print(colored("password must be at least 10 characters long", "red"))
-    if not (re.match(capital, password)):
-        print(colored('your password must include one capital letter at least', "red"))
-    if not (re.match(lowerCase, password)):
-        print(colored('your password must include one lower Case letter at least', "red"))
-    if not (re.match(number, password)):
-        print(colored('your password must include one digit at least', "red"))
+    else:
+        print(colored("password is invalid", 'red'))
+        # if(re.match(speciel,password)):
+        #     print(colored('no special characters are allowd', "red"))
+        if(len(password)<10):
+            print(colored("password must be at least 10 characters long", "red"))
+        if not (re.match(capital, password)):
+            print(colored('your password must include one capital letter at least', "red"))
+        if not (re.match(lowerCase, password)):
+            print(colored('your password must include one lower Case letter at least', "red"))
+        if not (re.match(number, password)):
+            print(colored('your password must include one digit at least', "red"))
 
-    exit(1)
-
-
+        exit(1)
 
 
 
+
+is_valid(password)
